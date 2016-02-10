a6679b8709e84a5fa33e3879757899d32ccf807d
Thibault Marquand <titotix@free.fr>
2016-02-10 18:27:04 +0100

    Code clean up

--- parser_contents2json.py
+++ parser_contents2json.py
@@ -5,24 +5,12 @@ import simplejson as json
 def contents(file, hexa):
     PDF_ELEMENT_INDIRECT_OBJECT = 2
     oPDFParser = pdfparser.cPDFParser(file)
-    cntComment = 0
-    cntXref = 0
-    cntTrailer = 0
-    cntStartXref = 0
-    cntIndirectObject = 0
-    dicObjectTypes = {}
-    content_json_objs = []  # 9bplus
-
-    selectComment = True
-    selectXref = True
-    selectTrailer = True
-    selectStartXref = True
-    selectIndirectObject = True
+    content_json_objs = []
 
     while True:
         object = oPDFParser.GetObject()
         if object != None:
-            if object.type == PDF_ELEMENT_INDIRECT_OBJECT and selectIndirectObject:
+            if object.type == PDF_ELEMENT_INDIRECT_OBJECT:
                 content_json_objs.append(pdfparser.content2JSON(object, hexa))
         else:
             break
